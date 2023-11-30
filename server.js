26a2267c962e52bbd9b235eef0e1ae0496485dc2
BunBun <73380142+profbunbun@users.noreply.github.com>
2023-11-30 15:00:00 -0500

    createerror error

--- server.js
+++ server.js
@@ -41,12 +41,12 @@ const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
 })
 
-// 404 Error
-app.use((req, res, next) => {
-    next(createError(404))
-})
-app.use(function (err, req, res, next) {
-    console.error(err.message)
-    if (!err.statusCode) err.statusCode = 500
-    res.status(err.statusCode).send(err.message)
-})
+// // 404 Error
+// app.use((req, res, next) => {
+//     next(createError(404))
+// })
+// app.use(function (err, req, res, next) {
+//     console.error(err.message)
+//     if (!err.statusCode) err.statusCode = 500
+//     res.status(err.statusCode).send(err.message)
+// })
